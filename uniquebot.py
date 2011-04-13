21c559ef0323f525aaa2f55a7ebfca10959a0ba4
robbo <dcchut@gmail.com>
2011-04-13 23:02:03 +1000

    first commit

---
+++ uniquebot.py
@@ -0,0 +1,92 @@
+# Copyright (c) Twisted Matrix Laboratories.
+# See LICENSE for details.
+
+# twisted imports
+from twisted.words.protocols import irc
+from twisted.internet import reactor, protocol
+from twisted.python import log
+
+# system imports
+import time, sys, sqlite3, hashlib
+
+# make sure only unique things are said
+class UniqueBot(irc.IRCClient):
+	nickname = "robbbot"
+	have_joined = False
+	
+	def noticed(self, user, channel, msg):
+		if (self.have_joined):
+			return
+						
+		# want to wait until we are identified to get auto-ops
+		# (i'm so lazy)
+		if 'You are now identified' in msg:
+			self.join(self.factory.channel)
+			self.have_joined = True
+			
+		print channel, user, msg
+		
+	def signedOn(self):
+		self.msg("nickserv", "identify " + self.factory.password)
+	
+	def privmsg(self, user, channel, msg):
+		if channel == self.nickname:
+			# PM's can blow me
+			return
+		
+		user = user.split('!', 1)[0]
+		
+		# take the lowercase version of the msg & remove trailing spaces & numbers
+		msg = msg.lower().rstrip().rstrip("0123456789").rstrip()
+		
+		# hash dat shit
+		hash = str(hashlib.sha224(msg).hexdigest())
+		
+		# has this been said before?
+		self.factory.c.execute("SELECT u FROM said WHERE t = ?", (hash,))
+		
+		for row in self.factory.c:
+			# kick dat ass
+			uk = row[0].encode('ascii', 'ignore')
+			self.kick("#newvce", user, "repeated: "+uk+" ("+hash[0:10]+")")
+			return
+				
+		# insert the text into the db
+		self.factory.c.execute("INSERT INTO said (u, t) VALUES (?,?)",(user,hash,))
+		self.factory.db.commit()
+
+class UniqueBotFactory(protocol.ClientFactory):
+	protocol = UniqueBot
+	
+	def __init__(self, password, channel, db):
+		self.channel = channel
+		self.password = password
+		
+		# open a "connection" to the sqlite db
+		self.db = sqlite3.connect(db)
+		
+		# (maybe) create the table
+		self.c = self.db.cursor()
+		self.c.execute("CREATE TABLE IF NOT EXISTS said(u TEXT, t TEXT)")
+		self.db.commit()
+	
+	def clientConnectionLost(self, connector, reason):
+		# reconnect if disconnected
+		connector.connect()
+	
+	def clientConnectionFailed(self, connector, reason):
+		print "connection failed:", reason
+		reactor.stop()
+
+if __name__ == '__main__':
+    # initialize logging
+    log.startLogging(sys.stdout)
+    
+    # create factory protocol and application
+    f = UniqueBotFactory("secret", "#newvce", "db")
+
+    # connect factory to this host and port
+    reactor.connectTCP("irc.freenode.net", 6667, f)
+
+    # run bot
+    reactor.run()
