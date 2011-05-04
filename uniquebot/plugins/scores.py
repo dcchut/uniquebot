9f35b47a472d0a948500c1290a9c8e72479644fe
robbo <dcchut@gmail.com>
2011-05-04 23:25:51 +1000

    hide 0 pointers when doing !scores

--- uniquebot/plugins/scores.py
+++ uniquebot/plugins/scores.py
@@ -18,6 +18,9 @@ class Plugin(CorePlugin):
 			users = bot.getAllUsers()
 			
 			for row in users:
+                if row.points == 0:
+                    continue
+                
 				bot.notice(user, "{0}: {1} points".format(row.user,row.points))
 			return True
 			
