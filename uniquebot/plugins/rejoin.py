5b815d9d303ae6e4f089d47eb510a5bba2ea57c1
robbo <dcchut@gmail.com>
2011-05-07 14:18:02 +1000

    made rejoin script check for auth

--- uniquebot/plugins/rejoin.py
+++ uniquebot/plugins/rejoin.py
@@ -2,6 +2,6 @@ from uniquebot.core.plugin import CorePlugin
 
 class Plugin(CorePlugin):
 	def incoming(self, user, hostname, channel, msg, current_time, bot):
-		if msg == "!rejoin":
+		if msg == "!rejoin" and bot.isAuthedUser(user):
 			bot.leave(self.factory.channel)
 			bot.join(self.factory.channel)
