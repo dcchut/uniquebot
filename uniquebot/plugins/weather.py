e90a8952867120b16b5ea65343106cf338179c18
robbo <dcchut@gmail.com>
2012-01-02 10:39:57 +1100

    changed the way the weather plugin works (now timer based, doesn't rely on user input!)

--- uniquebot/plugins/weather.py
+++ uniquebot/plugins/weather.py
@@ -1,23 +1,37 @@
 from uniquebot.core.plugin import CorePlugin
 from urllib import urlopen
+from twisted.internet import task
+from time import time
 
 class Plugin(CorePlugin):
-    last_weather_time = 0
-    last_weather_temp = 0
-    
+    bot = False
+    channels = {}
+
     def gettemp(self):
         wt = urlopen("http://www.baywx.com/melbtemp.html")
         
         # magic :D
         return float(wt.readlines()[3][8:].split('&')[0])
     
-    def incoming(self, user, hostname, channel, msg, current_time, bot):
-        if self.last_weather_time == 0:
-            self.last_weather_time = current_time
-            self.last_weather_temp = self.gettemp()
-        elif (current_time - self.last_weather_time > 60 * 25):
-            newtemp = self.gettemp()
-            if (newtemp != self.last_weather_temp):
-                self.last_weather_time = current_time
-                bot.say(channel, "weather update: " + str(self.last_weather_temp) + " to " + str(newtemp) + " degrees")
-                self.last_weather_temp = newtemp
+    def register(self,bot):
+        # add our channel to be tracked
+        self.channels[self.factory.channel] = [int(time()), self.gettemp()]
+        self.bot = bot
+        
+        # run our weather updater every 5 minutes
+        task.LoopingCall(self.update).start(60*5,False)
+    
+    def update(self):
+        current_time = int(time())
+        
+        # do we need a weather update?
+        for channel in self.channels:
+            cd = self.channels[channel]
+            
+            # has 25 minutes elapsed?
+            if (current_time - cd[0] > 60*25):
+                newtemp = self.gettemp()
+                if (newtemp != cd[1]):
+                    cd[0] = current_time
+                    self.bot.say(channel, "weather update: " + str(cd[1]) + " to " + str(newtemp) + " degrees")
+                    cd[1] = newtemp
