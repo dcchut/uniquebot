469c57a875111c47e0d4932fe80d149064dd13c8
robbo <dcchut@gmail.com>
2014-08-13 11:17:09 +1000

    removed auth/rejoin plugins

--- uniquebot/plugins/rejoin.py
+++
@@ -1,7 +0,0 @@
-from uniquebot.core.plugin import CorePlugin
-
-class Plugin(CorePlugin):
-	def incoming(self, user, hostname, channel, msg, current_time, bot):
-		if msg == "!rejoin" and bot.isAuthedUser(user):
-			bot.leave(self.factory.channel)
-			bot.join(self.factory.channel)
